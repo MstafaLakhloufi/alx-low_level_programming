43e6809b13e5ff054079e26db733f3effb8a08b9
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-16 16:54:31 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,23 +5,23 @@
 /**
  * main - Entry point
  *
- * Description: false or true validation
+ * Description: fales or true validation
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-int n;
+        int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
- 
-return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+                printf("%i is positive\n", n);
+        else if (n == 0)
+                printf("%i is zero\n", n);
+        else
+                printf("%i is negative\n", n);
+
+        return (0);
 }
