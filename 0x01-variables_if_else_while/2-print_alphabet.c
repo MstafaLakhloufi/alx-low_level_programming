310b630e76096a2aedfec3c616e09fcf9410eed8
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-16 18:48:16 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                putchar(ch);
-                ch++;
-        }
-        putchar('\n');
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
