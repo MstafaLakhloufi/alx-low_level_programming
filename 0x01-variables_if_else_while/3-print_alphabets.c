38752a5e0af47bf0d70c14086cd4cffeacf692f7
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-16 18:59:54 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,30 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: print the alphabet in lowercase, then in uppercase
+ * Description: print all alphabet letters
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
-	/*print a - z */
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-
-	/*print A - Z */
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		CH++;
-	}
-
 	putchar('\n');
 
 	return (0);
