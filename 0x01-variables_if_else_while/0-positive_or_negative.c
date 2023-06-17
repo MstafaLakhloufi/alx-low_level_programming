f0f26af8ef8a1c5a12829ae0a460ac9fdd4d4eda
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-16 18:19:16 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,26 +3,25 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers.
- * Return: 0 to exit
+ * main - Entry point
+ *
+ * Description: fales or true validation
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-int n;
+        int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{	
-        printf("%d is positive\n", n);
-}	
-else if (n == 0)
-{	
-        printf("%d is zero\n", n);
-}	
-else
-{	
-        printf("%d is negative\n", n);
-}	
-return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+                printf("%i is positive\n", n);
+        else if (n == 0)
+                printf("%i is zero\n", n);
+        else
+                printf("%i is negative\n", n);
+
+        return (0);
 }
