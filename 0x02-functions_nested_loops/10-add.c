3f537e6f6322a004b3e4710823214b7d1f8b9d80
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-20 16:22:45 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @num1: firdt number
+ * @num2: second number
+ * Return: Integer value
+ */
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
