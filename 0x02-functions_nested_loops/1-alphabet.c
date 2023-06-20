629dd21e3666a7a5f329199b2c41c4dd57188c3a
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-20 12:29:30 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print the alphabet a - z
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
