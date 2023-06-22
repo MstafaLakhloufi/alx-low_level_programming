232b8147015a2b5a48eae148455440efd9cea98c
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-22 09:54:43 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,17 @@
 
 void more_numbers(void)
 {
-	int row, num, count;
+	int row, num;
 
-	for (row = 1; row <= 10; row++)
+	for (row = 0; row < 10; row++)
 	{
-		for (count = 0; count <= 14; count++)
+		for (num = 0; num <= 14; num++)
 		{
-			num = count;
-			if (count > 9)
+			if (num > 9)
 			{
-				_putchar(1 + 48);
-				num = count % 10;
+				_putchar((num / 10) + '0');
 			}
-			_putchar(num + 48);
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
