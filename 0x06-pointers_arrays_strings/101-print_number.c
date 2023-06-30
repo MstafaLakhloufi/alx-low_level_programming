a12aca85a24fa3110b103a2cd98d40feaea0d100
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-29 20:05:43 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - print an intrger
- * @n: integer parameter
+ * print_number - print numbers chars
+ * @n: integer params
  * Return: 0
  */
 
@@ -22,5 +22,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
