9650c447ac1fcc481d62d92e2a3442a70e9de932
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-30 16:07:04 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,7 +4,6 @@
  * print_number - prints an integer;
  * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
@@ -13,12 +12,13 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
-	}
-	else
+	} else
 	{
 		n1 = n;
 	}
-	if (n1 / 10 != 0)
+
+	if (n1 / 10)
 		print_number(n1 / 10);
+
 	_putchar((n1 % 10) + '0');
 }
