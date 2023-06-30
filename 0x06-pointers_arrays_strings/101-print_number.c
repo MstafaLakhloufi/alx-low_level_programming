a532fd7c38e2c89781eea02ae278cc69b463e1ab
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-06-30 10:15:08 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,11 +6,6 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 
 	if (n < 0)
 	{
@@ -18,7 +13,7 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10)
 		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
