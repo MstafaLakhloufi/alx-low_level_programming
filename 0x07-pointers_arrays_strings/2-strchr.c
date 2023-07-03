dde7ea0b82ed0ba2a1217e9eb3a87544c3a5dee4
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-03 11:44:37 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int x;
 
-	for (x = 0; s[x] >= '\0'; x++)
+	for (x = 0; s[x] >= '\0' ; x++)
 	{
 		if (s[x] == c)
 			return (s + x);
 	}
-	return (NULL);
+	return ('\0');
 }
