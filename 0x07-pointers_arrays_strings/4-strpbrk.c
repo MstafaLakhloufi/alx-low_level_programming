e5a29dc21beea621b49aa7a7f1e4d062f83438f7
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-03 12:05:36 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+				return (s + x);
+		}
+	}
+	return (NULL);
+}
