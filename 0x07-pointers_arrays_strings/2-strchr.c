998376c2c0c02b7e82f510a82729db2e5132b0a8
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-03 11:33:04 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer input
+ * @c: input
+ * Return: Always 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (s + x)
+	}
+	return (NULL);
+}
