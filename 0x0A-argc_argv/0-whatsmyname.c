807a729094f0d6b83f746ea31e9f4e2c3fecca05
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-07 18:07:21 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: int
+ * @argv: int
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
