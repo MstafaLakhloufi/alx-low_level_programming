adad2ee05fc5657d1179cbdfc2b21ba1b187fe75
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-07-13 08:54:33 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 #define ERR_MSG "Error"
 
@@ -8,15 +10,16 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
+
 int is_digit(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (s[x])
+	while (s[i])
 	{
-		if (s[x] < '0' || s[x] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		x++;
+		i++;
 	}
 	return (1);
 }
@@ -27,20 +30,23 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
-	int x = 0;
 
-	while (s[x] != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		x++;
+		i++;
 	}
-	return (x);
+	return (i);
 }
 
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
 	printf("Error\n");
@@ -54,44 +60,44 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	char *str1, *str2;
-	int len1, len2, len, x, carry, digit1, digit2, *result, y = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	str1 = argv[1], str2 = argv[2];
-	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _puts(str1);
-	len2 = _puts(str2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (x = 0; x <= len1 + len2; x++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = str1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _puts(str2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = str2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
-	}
-	for (x = 0; x < len - 1; x++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[x])
-			y = 1;
-		if (y)
-			_putchar(result[x] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!y)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
