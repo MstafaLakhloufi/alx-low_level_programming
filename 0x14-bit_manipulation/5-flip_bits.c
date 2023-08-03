d5e6cb12cd2746f6883da05e8e55022cdae3bf35
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2023-08-03 16:23:57 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -17,7 +17,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (x = 63; x >= 0; x--)
 	{
-		curr = exclusive >> i;
+		curr = exclusive >> x;
 		if (curr & 1)
 			counter++;
 	}
