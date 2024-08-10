dcb3f2914544ece98a85715cd36d906a972a78dc
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-08-10 07:05:38 -0700

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,31 @@
 #include "search_algos.h"
 
 /**
-  * linear_search - function tolook for a value in an arrray of integers
+  * linear_search - function tolook for a value in
+  * an arrray of integers
+  *
   * @array: ptr to the first element  in the array.
   * @size: number of elements in the array.
   * @value: Value to search for.
+  *
   * Return: (-1) If value is not in array or array is NULL.
-  *         Otherwise, first index of value
+  * Otherwise, first index of value
   * Description: Print each value in the array as it gets compared.
   */
 int linear_search(int *array, size_t size, int value)
 {
-	int idx;
+	size_t i = 0;
 
-	if (array == NULL)
-		return (-1);
-
-	for (idx = 0; idx < (int)size; idx++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%u] = [%d]\n", idx, array[idx]);
-		if (value == array[idx])
-			return (idx);
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
 	}
 	return (-1);
 }
